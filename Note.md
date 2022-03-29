37817ceaaf8d65bfea866c2b7fd95652e2b06764
Régis BRUNET BLEC <regis.blec@gmail.com>
2022-03-29 20:17:45 +0200

    installation de la dépendance gh-pages

---
+++ Note.md
@@ -0,0 +1,17 @@
+# Mettre en ligne une app React sur GH Pages
+
+## Installation de la dépendance `gh-pages`
+
+lien npm : <https://www.npmjs.com/package/gh-pages>
+
+```bash
+npm i -D gh-pages
+
+yarn add -D gh-pages
+```
+
+## Création de l'adresse 
+
+```bash
+
+```
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Test pratique React
+        </h1>
+        <p>
+          Déploiement d'une application React sur GHPages.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
